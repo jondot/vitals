949e2cbbc25e3e022d82d6e741485321f07c8d91
Dotan Nahum <jondotan@gmail.com>
2016-03-21 23:32:52 +0200

    .

--- spec/multiverse_spec.rb
+++ spec/multiverse_spec.rb
@@ -4,13 +4,19 @@ require 'spec_helper'
 if ENV['INTEGRATION']
   describe "multiverse" do
     it "rails 4.2" do
-      system('cd spec/multiverse/rails42_app && bundle install --quiet && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake test').must_equal true
+      Dir.chdir('spec/multiverse/rails42_app') do
+        system('bundle install --quiet && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake test').must_equal true
+      end
     end
     it "grape-rack" do
-      system('cd spec/multiverse/grape-on-rack && bundle install --quiet && bundle exec rake spec').must_equal true
+      Dir.chdir('spec/multiverse/grape-on-rack') do
+        system('bundle install --quiet && bundle exec rake spec').must_equal true
+      end
     end
     it "grape-rails" do
-      system('cd spec/multiverse/grape-on-rails && bundle install --quiet && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake spec').must_equal true
+      Dir.chdir('spec/multiverse/grape-on-rails') do
+        system('bundle install --quiet && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake spec').must_equal true
+      end
     end
     it "padrino" do
       # TODO this should test the rack middleware, and padrino uses sinatra so that's good
