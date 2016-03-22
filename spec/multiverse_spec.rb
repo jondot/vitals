522049966a7b73e967fb6ab55eed603eb83a313e
Dotan Nahum <jondotan@gmail.com>
2016-03-22 14:13:24 +0200

    .

--- spec/multiverse_spec.rb
+++ spec/multiverse_spec.rb
@@ -9,29 +9,22 @@ if ENV['INTEGRATION']
     it "rails 4.2" do
       Bundler.with_clean_env do
         Dir.chdir('spec/multiverse/rails42_app') do
-          ignore_bundle = 'BUNDLE_IGNORE_CONFIG=1 BUNDLE_GEMFILE=`pwd`/Gemfile '
-          sh("#{ignore_bundle}install && #{ignore_bundle}RAILS_ENV=test bundle exec rake db:migrate && #{ignore_bundle}bundle exec rake test").must_equal true
+          sh("bundle install && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake test").must_equal true
         end
       end
     end
     it "grape-rack" do
       Bundler.with_clean_env do
-      Dir.chdir('spec/multiverse/grape-on-rack') do
-        system('pwd')
-        system('ls')
-        system('bundle config')
-        system('bundle install && bundle exec rake spec').must_equal true
-      end
+        Dir.chdir('spec/multiverse/grape-on-rack') do
+          system('bundle install && bundle exec rake spec').must_equal true
+        end
       end
     end
     it "grape-rails" do
       Bundler.with_clean_env do
-      Dir.chdir('spec/multiverse/grape-on-rails') do
-        system('pwd')
-        system('ls')
-        system('bundle config')
-        system('bundle install && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake spec').must_equal true
-      end
+        Dir.chdir('spec/multiverse/grape-on-rails') do
+          system('bundle install && bundle exec rake spec').must_equal true
+        end
       end
     end
     it "padrino" do
