b4b42673c0ca98bfd9d16d70f24d1706fe720822
Dotan Nahum <jondotan@gmail.com>
2016-03-22 08:41:47 +0200

    .

--- spec/multiverse_spec.rb
+++ spec/multiverse_spec.rb
@@ -5,16 +5,22 @@ if ENV['INTEGRATION']
   describe "multiverse" do
     it "rails 4.2" do
       Dir.chdir('spec/multiverse/rails42_app') do
+        system('pwd')
+        system('ls')
         system('bundle install --quiet && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake test').must_equal true
       end
     end
     it "grape-rack" do
       Dir.chdir('spec/multiverse/grape-on-rack') do
+        system('pwd')
+        system('ls')
         system('bundle install --quiet && bundle exec rake spec').must_equal true
       end
     end
     it "grape-rails" do
       Dir.chdir('spec/multiverse/grape-on-rails') do
+        system('pwd')
+        system('ls')
         system('bundle install --quiet && RAILS_ENV=test bundle exec rake db:migrate && bundle exec rake spec').must_equal true
       end
     end
